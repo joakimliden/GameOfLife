2de669681a779f8a7d509b9fb972cb28fc24dd65
joakimliden <joakim.liden@iths.se>
2021-11-05 15:31:24 +0100

    checkingThatPrintGridMethodHasExpectedOutcomeForEmptyBoard test passes

--- src/main/java/se/joakimliden/ConsolePrint.java
+++ src/main/java/se/joakimliden/ConsolePrint.java
@@ -2,6 +2,19 @@ package se.joakimliden;
 
 public class ConsolePrint {
     public String printGrid(Board board) {
-        return "";
+        StringBuilder line = new StringBuilder();
+        line.append("Generation " + board.getGridGeneration() + "\n");
+        for (int y = 0; y < board.getHeight(); y++) {
+            for (int x = 0; x < board.getWidth(); x++) {
+                if (board.getGrid()[x][y] == 0) {
+                    line.append("[ ]");
+                } else {
+                    line.append("[*]");
+                }
+            }
+            line.append("\n");
+        }
+        System.out.println(line);
+        return line.toString();
     }
 }
