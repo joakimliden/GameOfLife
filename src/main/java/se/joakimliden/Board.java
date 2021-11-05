b652b84b6cf921a89b84b80cf70240eb18407c08
joakimliden <joakim.liden@iths.se>
2021-11-05 14:02:50 +0100

    Adds grid field in Board

--- src/main/java/se/joakimliden/Board.java
+++ src/main/java/se/joakimliden/Board.java
@@ -1,6 +1,9 @@
 package se.joakimliden;
 
 public class Board {
+    
+    public int[][] grid;
+
     public Board(int width, int height) {
     }
 }
--- src/test/java/se/joakimliden/BoardTest.java
+++ src/test/java/se/joakimliden/BoardTest.java
@@ -1,6 +1,11 @@
 package se.joakimliden;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BoardTest {
 
@@ -9,5 +14,11 @@ public class BoardTest {
     @BeforeEach
     void setUp() {
         board = new Board(5, 8);
+    }
+
+    @Test
+    void givenANewBoardShouldReturnNewGrid() {
+
+        String expected = Arrays.deepToString(board.grid);
     }
 }
