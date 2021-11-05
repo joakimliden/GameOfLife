fd77e302a44fe857a786d4b17e6da98062b193a6
joakimliden <joakim.liden@iths.se>
2021-11-05 14:10:17 +0100

    givenANewBoardShouldNotReturnExactlyTheSameGrid test passes

--- src/main/java/se/joakimliden/Board.java
+++ src/main/java/se/joakimliden/Board.java
@@ -2,12 +2,16 @@ package se.joakimliden;
 
 public class Board {
 
+    private final int width;
+    private final int height;
     public int[][] grid;
 
     public Board(int width, int height) {
+        this.width = width;
+        this.height = height;
     }
 
     public int[][] nextGrid() {
-        return this.grid;
+        return new int[width][height];
     }
 }
